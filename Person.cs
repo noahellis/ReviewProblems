c10effaa027b7f1c15bc2907939521ae3c4d1c33
noahellis <noah_ellis@hotmail.com>
2016-04-12 17:32:49 -0500

    start of the person problem, but no funtion to run it

---
+++ Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewProblems
+{
+    class Person
+    {
+        static string firstName;
+        static string lastName;
+        static int age;
+        static string birthday;
+        static int addedAge = 12;
+
+        public Person(string Birthday)
+        {
+            birthday = Birthday;
+        }
+        public Person()
+        {
+
+        }
+        public Person (string FirstName, string LastName, int Age)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            age = Age;
+        }
+        public static Person operator + (Person person, int age)
+        {
+            Person newPerson = new Person();
+            age = age + addedAge;
+            Console.WriteLine("New Person: {0}{1}, Age in {3} years: {4}", firstName, lastName, addedAge, age);
+            return person;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,11 +10,13 @@ namespace ReviewProblems
     {
         static void Main(string[] args)
         {
-            CustomIteratorProblem testCustomIterator = new CustomIteratorProblem();            
-            foreach (string flightData in testCustomIterator)
-            {
-                Console.Write(flightData);
-            }
+            //Person person = new Person("Noah", "Ellis", 31);
+            
+            //CustomIteratorProblem testCustomIterator = new CustomIteratorProblem();            
+            //foreach (string flightData in testCustomIterator)
+            //{
+            //    Console.Write(flightData);
+            //}
             Console.ReadKey();
         }
     }
